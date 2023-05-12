6b95db2bcf19c2652536a08055456c6917c00007
IlyuAfanasev <gecktor.86@mail.ru>
2023-05-12 23:00:10 +0400

    compliting task

--- HMExceptions/src/Main.java
+++ HMExceptions/src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        check("dhhr_1","jfklknfla_1","jfklknfla_1");
+
+    }
+    public static void check (String login, String password, String confirmPassword) {
+        try {
+            checkParams(login, password, confirmPassword);
+        } catch (WrongLoginException e) {
+            System.out.println("Некорректный  ввод данных параметра login");
+        } catch (WrongPasswordException e) {
+            System.out.println("Некорректный ввод данных параметра password или confirmPassword");
+        }
+        finally {
+            System.out.println("Проверка завершена");
+        }
+    }
+
+
+    public static void checkParams (String login, String password, String confirmPassword)
+            throws WrongLoginException, WrongPasswordException {
+        boolean validLogin = login.matches("\\w+");
+        if (!validLogin || login.length()>20) {
+            throw new WrongLoginException();
+        }
+
+        boolean validPassword = password.matches("\\w+");
+        if (!validPassword || password.length()>19) {
+            throw new WrongPasswordException();
+        }
+
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException();
+        }
     }
 }
