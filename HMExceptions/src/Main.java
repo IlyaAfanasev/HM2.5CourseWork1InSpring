5a910d65c6bdae3c677d35fd7ecd5e8f07af2103
IlyuAfanasev <gecktor.86@mail.ru>
2023-05-12 23:01:39 +0400

    completing task

--- HMExceptions/src/Main.java
+++ HMExceptions/src/Main.java
@@ -1,32 +1,32 @@
 public class Main {
     public static void main(String[] args) {
 
-        check("dhhr_1","jfklknfla_1","jfklknfla_1");
+        check("dhhr_1", "jfklknfla_1", "jfklknfla_1");
 
     }
-    public static void check (String login, String password, String confirmPassword) {
+
+    public static void check(String login, String password, String confirmPassword) {
         try {
             checkParams(login, password, confirmPassword);
         } catch (WrongLoginException e) {
             System.out.println("Некорректный  ввод данных параметра login");
         } catch (WrongPasswordException e) {
             System.out.println("Некорректный ввод данных параметра password или confirmPassword");
-        }
-        finally {
+        } finally {
             System.out.println("Проверка завершена");
         }
     }
 
 
-    public static void checkParams (String login, String password, String confirmPassword)
+    public static void checkParams(String login, String password, String confirmPassword)
             throws WrongLoginException, WrongPasswordException {
         boolean validLogin = login.matches("\\w+");
-        if (!validLogin || login.length()>20) {
+        if (!validLogin || login.length() > 20) {
             throw new WrongLoginException();
         }
 
         boolean validPassword = password.matches("\\w+");
-        if (!validPassword || password.length()>19) {
+        if (!validPassword || password.length() > 19) {
             throw new WrongPasswordException();
         }
 
