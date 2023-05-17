c791ada6ed5803e556bc9126e6e2aa0cf3ab496a
IlyuAfanasev <gecktor.86@mail.ru>
2023-05-17 18:47:57 +0400

    initial commit

---
+++ HM2_5CourseWork1InSpring/src/main/java/pro/sky/java/course2/hm2_5coursework1inspring/Hm25CourseWork1InSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hm2_5coursework1inspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hm25CourseWork1InSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hm25CourseWork1InSpringApplication.class, args);
+    }
+
+}
